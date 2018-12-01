0d28b8c1b1e71817c595eda275a0c634b6f33359
Ildi Bilali <bilaliildi@gmail.com>
2018-12-01 01:55:25 -0500

    finished transaction by date

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -1,20 +1,33 @@
-const transactionRouter = require.apply('express').Router;
-const { Transaction, LineItem, Product } = require('../db/models');
+const transactionRouter = require('express').Router();
+const { Transaction, LineItem, User, Product } = require('../db/models');
+
 
 //search transactions by date
+
 transactionRouter.get('/:date', (req, res, next) => {
-   Transaction.find({
-       where: {date: req.params.date },
-       include: [{ model: Product }, {model: LineItem}]
+   Transaction.findAll({
+       where: {placedAt: req.params.date },
+       include: {model: User}
    })
-   .then(transaction => res.json(transaction))
-   .catch(next);
+   .then(transactions => {
+       const transactionDetail = transactions.map(transaction => {
+           return transaction.getDetails()
+        })
+        return Promise.all(transactionDetail).then(x => {
+            const det = x.map((lineItems, i) => {
+                const obj = Object.assign({}, { lineItems }, {buyerName: transactions[i].user.name})
+                return obj;
+            })
+            res.json(det)
+        })
+    })
+    .catch(next);
 });
 
 //create new transaction
 transactionRouter.post('/', (req, res, next) => {
     Transaction.create(req.body)
-    .then(instance => res.json(instance))
+    .then(instance => LineItem.bulkCreate(req.body.orders))
     .catch(next);
 });
 
--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const LineItem = require('./lineItem');
+const Product = require('./product');
 
 const Transaction = db.define('transaction', {
     placedAt: {
@@ -7,5 +9,13 @@ const Transaction = db.define('transaction', {
         defaultValue: Sequelize.NOW
     }
 });
+
+Transaction.prototype.getDetails = function(){
+    return LineItem.findAll({
+        where: { transactionId: this.id },
+        include: { model: Product }    
+    })
+    .catch(err => console.error(err));
+};
 
 module.exports = Transaction;
